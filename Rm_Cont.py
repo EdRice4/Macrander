73da8a821ffe0bed3e49705c5a74e8d583e21b81
EdRice4 <edwinricethe4th@gmail.com>
2015-07-31 14:03:55 -0400

    Formatting.
    Clarify purpose of scripts, argument parsing, folding, comments.

--- Rm_Cont.py
+++ Rm_Cont.py
@@ -8,60 +8,91 @@ import argparse
 # {{{ RegEx
 class RegEx(object):
 
-    """A class in which regular expression functionality is stored."""
+    """ {{{ Docstrings
+    A class in which regular expression functionality is stored.
+    }}} """
 
+    # {{{ strip_and_escape
     def strip_and_escape(self):
         rm_strip_esc = map(lambda x: re.escape(x.strip()), self.rm)
         return rm_strip_esc
+    # }}}
 
+    # {{{ compile_rm
     def compile_rm(self):
         pattern = self.strip_and_escape()
         pattern = re.compile('|'.join(pattern))
         return pattern
+    # }}}
 
+    # {{{ remove_cont
     def remove_cont(self):
         pattern = self.compile_rm()
         for i in self.fas:
             if bool(pattern.match(i)):
                 self.fas.remove(i)
+    # }}}
+# }}}
 
 
+# TODO: What sort of files am I working with here?
+# {{{ FileIO
 class FileIO(RegEx):
 
-    """A class in which file input/output is stored."""
+    """ {{{ Docstrings
+    A class in which file input/output is stored.
+    }}} """
 
-    def write_new_fasta(self):
+    # {{{ write_new_output
+    def write_new_output(self):
         new_fasta_seq = '>'.join(self.fas)
         with open(self.new_fasta, 'w') as fas:
             fas.write('>')
             fas.write(new_fasta_seq)
+    # }}}
+# }}}
 
 
+# {{{ IterRegistry
 class IterRegistry(type):
 
-    """A metaclass allowing for iterations over instances of the FastaFile
-       class."""
+    """ {{{ Docstrings
+    A metaclass allowing for iterations over instances of the FastaFile
+    class.
+    }}} """
 
+    # {{{ __iter__
     def __iter__(cls):
         return iter(cls.registry)
+    # }}}
+# }}}
 
 
+# {{{ FastaFile
 class FastaFile(FileIO):
 
-    """A class in which all the necessary parameters corresponding to each
-       respective fasta file are stored."""
+    """ {{{ Docstrings
+    A class in which all the necessary parameters corresponding to each
+    respective fasta file are stored.
+    }}} """
 
+    # {{{ __metaclass__
     __metaclass__ = IterRegistry
     registry = []
+    # }}}
 
+    # {{{ __init__
     def __init__(self, fasta_file, rem_file):
         with open(fasta_file, 'r') as fas:
             fas = fas.read()
             self.fas = (fas[1:]).split('>')
-        self.new_fasta = fasta_file.replace('.fasta', '_new.fasta')
+        # TODO: What sort of files am I working with here?
+        self.decont = fasta_file.replace('.fasta', '_new.fasta')
         with open(rem_file, 'r') as rem:
             self.rm = rem.readlines()
         self.registry.append(self)
+    # }}}
+# }}}
 
 # {{{ ArgParser
 arg_parser = argparse.ArgumentParser(
@@ -106,6 +137,7 @@ args = arg_parser.parse_args()
 if args.batch:
     cwd = getcwd()
     fid = listdir(cwd)
+    # TODO: What sort of files am I working with here?
     fasta_files = sorted([x for x in fid if '.fasta' in x])
     rem_files = sorted([x for x in fid if '.txt' in x])
     for i, j in zip(fasta_files, rem_files):
