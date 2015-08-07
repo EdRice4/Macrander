9dd85232e3d5d0ed0c6072677bb979580e297d55
EdRice4 <edwinricethe4th@gmail.com>
2015-08-07 17:11:18 -0400

    Modified SearchParse block.
    Added filter_pep_dict function.
    Modified search_the_3_Cs:
    
        Now search the 6 Cs.
        Accepts string as argument rather than
            dictionary.

--- ShK.py
+++ ShK.py
@@ -71,32 +71,36 @@ class SearchParse(PepFile):
     # "|" in regular expression speak means "or"
     amino_acids = '|'.join(amino_acids)
     # "{m}" in regular expression speak means "match exactly this number of
-    # preceding expression
+    # preceding expression"
     aa_pattern = (
             'C[' + amino_acids + ']{3}C[' + amino_acids + ']{2}C'
             )
     # Compile into pattern re.search can utilize
     aa_pattern = re.compile(amino_acids)
 
-    # {{{ search_the_3_Cs (Arr, matey)
-    def search_the_3_Cs(self, pep_dict):
+    # {{{ search_the_6_Cs (Arr, matey)
+    def search_the_6_Cs(self, pep_seq):
 
         """ {{{ Docstrings
 
         Utilizing regular expressions (as provided by the "re" python module),
-        iterates over the values of the pep_dict, searching for pattern:
-        C???C??C where ? = any amino acid.
+        searches a peptide sequence for the pattern: C???C??C where ? = any
+        amino acid.
 
         }}} """
 
+        contains_shk = re.search(aa_pattern, v)
+        if contains_shk:
+            filtered_pep_dict[k] = v
+    # }}}
+
+    # {{{ filter_pep_dict
+    def filter_pep_dict(self, pep_dict):
         # Initialze empty dictionary where peptide sequences containing the
         # aforementioned pattern will be stored
         filtered_pep_dict = {}
         # Itereate over keys, values in pep_dict
         for k, v in pep_dict.iteritems():
-            contains_shk = re.search(v, aa_pattern)
-            if contains_shk:
-                filtered_pep_dict[k] = v
     # }}}
 # }}}
 
