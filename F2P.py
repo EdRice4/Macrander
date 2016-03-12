af0aa916b421f87324884195eb5890ce35ba436e
EdRice4 <edwinricethe4th@gmail.com>
2016-03-12 12:42:43 -0500

    Fixed various syntax errors in F2P.py.

--- F2P.py
+++ F2P.py
@@ -287,15 +287,15 @@ class FastaFile(PhylipFile):
 arg_parser = argparse.ArgumentParser(
         prog='Fasta2Phylip.py',
         description=(
-                'Converts a fasta file to phylip format, either writing it in'
-                'sequential or interleaved format, depending on user'
+                'Converts a fasta file to phylip format, either writing it in '
+                'sequential or interleaved format, depending on user '
                 'specification. In total, 3 files will be produced: 1.) A '
                 'phylip file with original IDs. 2.) A phylip file with unique '
                 'IDs. 3.) A dictionary file with unique and original IDs.'
                 'The intended workflow is analgous to the following: '
                 'fasta_file >F2P.py> phylip_file_unique >Analysis> '
                 'output_unique >Rm_Cont.py> output_original. Designed to '
-                'circumvent the fact that many analysis requiring phylip '
+                'circumvent the fact that many analyses requiring phylip '
                 'files as input truncate IDs at 10 chracters.'
                 ),
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
